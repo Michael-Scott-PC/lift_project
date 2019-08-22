9e01dfb20394abef1530a63e4de90bf02a7f8908
Mike Enochs <msenochs@umich.edu>
2019-08-22 17:41:41 -0400

    Updated calendars page and css
    Decided to change the structure once more and go with css grid only - removed bootstrap grid columns.

--- calendars/views.py
+++ calendars/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .months_days import months, days, january, february, march, april, may, june, july, august, september, october, november, december
+from django.http import HttpResponse
 
 # Create your views here.
 def calendars(request):
@@ -24,4 +25,21 @@ def calendars(request):
     return render(request, 'calendars/calendars.html', context)
 
 def calendar(request):
-    return render(request, 'calendars/calendar.html')
+    context = {
+        'months': months,
+        'days': days,
+        'january': january,
+        'february': february,
+        'march': march,
+        'april': april,
+        'may': may,
+        'june': june,
+        'july': july,
+        'august': august,
+        'september': september,
+        'october': october,
+        'november': november,
+        'december': december
+    }
+    
+    return render(request, 'calendars/calendar.html', context)
