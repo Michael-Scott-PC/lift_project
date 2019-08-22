e4a2f9f4adf196d7016609f57b0ae8417ec7b1b2
Mike Enochs <msenochs@umich.edu>
2019-08-21 20:54:52 -0400

    Created the calendars page
    For now i've created the function that generates and retrieves the proper dates for each month outside of this program in a scratch pad and copy pasted the data into months_days.py.  I will eventually move the function into the program so it is ran on page load, along with other functions that generate the following years dates and the previous years dates.

--- calendars/views.py
+++ calendars/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from .months_days import months, days, january, february, march, april, may, june, july, august, september, october, november, december
 
 # Create your views here.
 def calendars(request):
-    return render(request, 'calendars/calendars.html')
+
+    context = {
+        'months': months,
+        'days': days,
+        'january': january,
+        'february': february,
+        'march': march,
+        'april': april,
+        'may': may,
+        'june': june,
+        'july': july,
+        'august': august,
+        'september': september,
+        'october': october,
+        'november': november,
+        'december': december
+    }
+
+    return render(request, 'calendars/calendars.html', context)
 
 def calendar(request):
     return render(request, 'calendars/calendar.html')
